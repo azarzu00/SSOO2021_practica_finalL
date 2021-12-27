eb9d2d88a008ae91606f5e609a23d818f909ebd6
Ana Zarzuelo <72804555+azarzu00@users.noreply.github.com>
2021-12-27 22:01:44 +0000

    primeros pasos

---
+++ main.c
@@ -0,0 +1,12 @@
+//Incluir las librerias
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+//pruebica
